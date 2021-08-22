85512eb8760566518399022e6f6ffd5c9e74f73a
devHikary <wrkkarina@gmail.com>
2021-08-22 14:13:50 -0300

    Aula 5.4 state

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -5,15 +5,27 @@ import FormularioCadastro from './components/FormularioCadastro/FormularioCadast
 
 class App extends Component {
 
+  constructor() {
+    super();    
+    this.state = {
+      notas: []
+    }
+  }
+
   criarNota(titulo, texto){
-    console.log(`Nova Nota foi criada ${titulo} ${texto}`)
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado)
   }
 
   render() {
     return (
     <section className="conteudo">
-      <FormularioCadastro criarNota={this.criarNota} />
-      <ListaDeNotas/>
+      <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+      <ListaDeNotas notas={this.state.notas}/>
     </section>
   );
   }  
--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from "react"
 import CardNota from '../CardNota/CardNota';
+import "./style.css"
 
 class ListaDeNotas extends Component {
   render() {
     return (
       <ul className="lista-notas">
-        {this.props.notas.map((categoria, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota titulo={nota.titulo} texto={nota.texto} />
             </li>
           );
         })}
