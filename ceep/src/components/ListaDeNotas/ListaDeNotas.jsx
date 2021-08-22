4bf341b4a6ca1bd2c9e9aa9d88fd57a2d056f28e
devHikary <wrkkarina@gmail.com>
2021-08-22 12:04:00 -0300

    Aula 2.5 Array de Notas

--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -5,10 +5,15 @@ export default class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        <li>
-          <CardNota/>
-        </li>
-      </ul >
+        {Array.of("Trabalho", "Trabalho", "Estudo").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNota />
+            </li>
+          );
+        })}
+      </ul>
     );
   }
 }
