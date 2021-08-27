fb53a7d76c1f28585d5c3bacfecf1907fcefe627
devHikary <wrkkarina@gmail.com>
2021-08-27 06:21:25 -0300

    2Aula 5.3

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,44 +2,56 @@ import React, { Component } from "react";
 import "./style.css";
 
 class FormularioCadastro extends Component {
-  constructor(props) {
+
+  constructor(props){
     super(props);
-    this.titulo = "";
-    this.texto = "";
+    this.titulo="";
+    this.texto="";
     this.categoria = "Sem Categoria";
+    this.state = {categorias:[]}
   }
 
-  _handleMudancaTitulo(evento) {
+  componentDidMount() {
+    this.props.categorias.inscrever(this._novasCategorias.bind(this));
+  }
+
+  _novasCategorias(categorias){
+    this.setState({...this.state, categorias});
+  }
+
+  _handleMudancaTitulo(evento){
     evento.stopPropagation();
     this.titulo = evento.target.value;
   }
 
-  _handleMudancaTexto(evento) {
+  _handleMudancaTexto(evento){
     evento.stopPropagation();
     this.texto = evento.target.value;
   }
 
-  _handleMudancaCategoria(evento) {
+  _handleMudancaCategoria(evento){
     evento.stopPropagation();
     this.categoria = evento.target.value;
   }
 
-  _criarNota(evento) {
+  _criarNota(evento){
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto, this.categoria);
+    this.props.criarNota(this.titulo, this.texto, this.categoria)
   }
 
   render() {
     return (
-      <form className="form-cadastro " onSubmit={this._criarNota.bind(this)}>
+      <form className="form-cadastro "
+        onSubmit={this._criarNota.bind(this)}
+      >
         <select
           onChange={this._handleMudancaCategoria.bind(this)}
           className="form-cadastro_input"
         >
           <option>Sem Categoria</option>
-          {this.props.categorias.map((categoria) => {
-            return <option>{categoria}</option>;
+          {this.state.categorias.map((categoria, index)=>{
+            return <option key={index}>{categoria}</option>
           })}
         </select>
         <input
--- ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -3,16 +3,30 @@ import CardNota from "../CardNota/CardNota";
 import "./style.css";
 
 class ListaDeNotas extends Component {
+
+  constructor(){
+    super();
+    this.state = {notas:[]}
+  }
+
+  componentDidMount() {
+    this.props.notas.inscrever(this._novaNotas.bind(this));
+  }
+
+  _novaNotas(notas) {
+    this.setState({...this.state, notas});
+  }
+
   render() {
     return (
       <ul className="lista-notas">
-        {this.props.notas.map((nota, index) => {
+        {this.state.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
               <CardNota
                 indice={index}
                 apagarNota={this.props.apagarNota}
-                titulo={nota.titulo}
+                titulo={nota.titulo} 
                 texto={nota.texto}
                 categoria={nota.categoria}
               />
